bb2afc4abb4407ce6006fd00625767b8d38a7aeb
Fares Kissoum <kissoumfares@gmail.com>
2019-10-27 00:56:54 +0200

    Mise en place de la persistence
    Ajout du JPA Hibernate et connexion avec la BDD MySQL

---
+++ app/src/main/java/Utilities/JPAUtil.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author fareskissoum
+ */
+public class JPAUtil {
+
+    private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE";
+    private static EntityManagerFactory factory;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return factory;
+    }
+
+    public static void shutdown() {
+        if (factory != null) {
+            factory.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+
+        // Check database version
+        String sql = "select version()";
+
+        String result = (String) entityManager.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+        JPAUtil.shutdown();
+    }
+
+}
