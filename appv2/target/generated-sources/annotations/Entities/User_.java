34f2847e388d9537a6e1730563c5eddf19554f8d
larry <larry.andriamampianina@efrei.net>
2019-11-17 11:51:29 +0100

    V2: ajout du css dans modify.jsp

--- appv2/target/generated-sources/annotations/Entities/User_.java
+++ appv2/target/generated-sources/annotations/Entities/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-16T20:35:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-16T21:51:46")
 @StaticMetamodel(User.class)
 public class User_ { 
 
